982802722853eaaa9d67266c4fd859bd74482229
Hong Pang <2014220902023@std.uestc.edu.cn>
2017-09-19 16:18:53 +0800

    add new code  pascal_tri

--- exc116.java
+++ exc116.java
@@ -9,6 +9,19 @@ public class exc116{
 		}
 		return g;
 	}
+	// Pascal triangle
+	// 1      --0
+	// 1 1    --1
+	// 1 2 1  --2
+	// 1 3 3 1--3
+	// 1 4 6 4 1--4
+	// .....
+	// 1 (n,1) (n,2)...(n,n) --n
+
+	// (n,1) = (n-1,0) + (n-1,1);
+	// (n,k) = (n-1, k-1) + (n-1,k); k E [0,n]
+
+	
 	// print test case
 	public static void print_test_case(boolean p){
 		StdOut.println("--TEST--");
